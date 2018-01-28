812c4d5bf57a18a9f3d1b31c4bd6c909abfc55dd
Hu Yuhang <yhhu2049@gmail.com>
2018-01-29 00:01:32 +0800

    learning on 2018.01.29

---
+++ 0127.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 27 22:38:26 2018
+f
+@author: hyh
+"""
+
+#with open('test/readme.txt') as file_object:
+#    contents = file_object.read()
+#    print(contents)
+filepath = 'D:/HelloWorld/learngit/test/readme.txt'
+with open(filepath) as name:
+    for hang in name:
+        print(hang)
+        
